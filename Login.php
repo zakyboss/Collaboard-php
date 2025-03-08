261bc5b7b44d89d6220bb2d5724d4b88db743e30
Zakyboss <Boochimo8@gmail.com>
2025-03-08 13:36:35 +0300

    Progress

--- Login.php
+++ Login.php
@@ -1,6 +1,6 @@
 <?php
 header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Methods: POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
@@ -55,3 +55,4 @@ if ($row = pg_fetch_assoc($result)) {
 http_response_code(200);
 echo json_encode($response, JSON_UNESCAPED_UNICODE);
 exit;
+?>
