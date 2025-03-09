ac24b0e082b36e3827dd6812080b637c3bdd31a4
Zakyboss <Boochimo8@gmail.com>
2025-03-09 14:18:58 +0300

    Progress

--- Login.php
+++ Login.php
@@ -1,5 +1,10 @@
 <?php
 // File: Collaboard-php/Login.php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+ini_set('html_errors', 0);
+
 
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: POST, OPTIONS");
