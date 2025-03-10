c4828aa0f9f4dd549ef82e0968249fed610e734c
Zakyboss <Boochimo8@gmail.com>
2025-03-10 15:23:59 +0300

    Progress

--- GetVolunteers.php
+++ GetVolunteers.php
@@ -21,20 +21,20 @@ $response = ["volunteers" => []];
 
 if ($proj_id > 0) {
     $sql = "
-        SELECT v.volunteer_id,
-               v.user_id,
-               v.github_username,
-               v.task_id,
-               v.is_approved,
-               v.created,
-               u.first_name,
-               u.last_name,
-               u.profile_photo
-        FROM collaboardtable_volunteers v
-        JOIN collaboardtable_users u ON v.user_id = u.id
-        WHERE v.proj_id = $1
-        ORDER BY v.created DESC
-    ";
+    SELECT v.volunteer_id,
+           v.user_id,
+           v.github_username,
+           v.task_id,
+           v.is_approved,
+           v.created,
+           u.first_name,
+           u.last_name,
+           u.profile_photo
+    FROM collaboardtable_volunteers v
+    JOIN collaboardtable_users u ON v.user_id = u.user_id
+    WHERE v.proj_id = $1
+    ORDER BY v.created DESC
+";
 
     $result = pg_query_params($conn, $sql, [$proj_id]);
     if (!$result) {
@@ -44,7 +44,7 @@ if ($proj_id > 0) {
         pg_close($conn);
         exit();
     }
-    
+
     while ($row = pg_fetch_assoc($result)) {
         $response["volunteers"][] = $row;
     }
